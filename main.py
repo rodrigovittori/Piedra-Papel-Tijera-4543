13192970c5e40469530c78a0b4e4d80d1ee5629c
Rodrigo Vittori <vittorirodrigo@gmail.com>
2024-12-21 13:57:56 -0300

    Paso 3: Pedir al Jugador su elección y validarla
    Pedimos al jugador su elección y nos aseguramos de que ingrese un valor válido

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 """
 Primera Actividad Módulo 3: Proyecto - Piedra-Papel-Tijeras
-Versión: 0.2.0
+Versión: 0.3.0
 -------------------------------------
 
-Paso Nº 2: Creamos el bucle principal:
-    > agregamos una nueva variable de control, de tipo bool (verdadero o falso) llamada seguir_jugando
-    > agregamos un bucle while(seguir_jugando)
-      y dentro de éste un check para validar si el usuario desea continuar o no
-      
+Paso Nº 3: Pedir al Jugador su elección (piedra, papel o tijera)
+    > Creamos una nueva variable (dentro del bucle) -> opcion_jugador
+    > Pedimos su valor con input() y lo convertimos a int()
+        > 1) Piedra
+        > 2) Tijeras
+        > 3) Papel
+
+    > Creamos un bucle para verificar que la opción ingresada sea válida (opcion_jugador >= 0) and (opcion_jugador <= 3)
+        > Si el valor NO es válido, lo solicitamos nuevamente
+        > En cambio, si es válido, convertimos ese valor a su equivalente en texto:
+            > 1: "Piedra" / 2: "Tijeras" / 3: "Papel"
 """
 
 """ ··· [ Variables ] ··· """
@@ -24,6 +30,27 @@ seguir_jugando = True
     ###################    """
 
 while (seguir_jugando):
+
+
+    """ > Paso Nº 1: Pedir al jugador su elección """
+    opcion_jugador = 0
+
+    while ((opcion_jugador <= 0) or (opcion_jugador > 3)):
+        opcion_jugador = int(input("\n Que eliges? \n (1) Piedra (2) Tijeras (3) Papel \n >"))
+
+    # Convertimos el valor ingresado por el Usuario a texto:
+
+    if (opcion_jugador == 1):
+        opcion_jugador = "Piedra"
+            
+    elif (opcion_jugador == 2):
+        opcion_jugador = "Tijeras"
+            
+    elif (opcion_jugador == 3):
+        opcion_jugador = "Papel"
+        
+    else:
+        opcion_jugador = "VALOR NO VALIDO"
     
     # Paso Final: PREGUNTAMOS SI QUIERE SEGUIR JUGANDO
 
